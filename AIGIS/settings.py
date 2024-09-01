52cc6fa152c8a7ab82e6d1575b7087227e52b936
James Ndubuisi <jamescndubuisi@gmail.com>
2024-09-02 00:28:35 +0100

    async

--- AIGIS/settings.py
+++ AIGIS/settings.py
@@ -153,8 +153,8 @@ GOOGLE_API = env("GOOGLE_API")
 
 
 # settings.py
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
@@ -162,9 +162,11 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = "Africa/Lagos"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BROKER_URL = 'amqp://localhost'
 # CELERY_BROKER_URL = 'amqp://localhost'
 
 
-# celery -A basicSetup.celery worker -l info
+# celery -A AIGIS.celery worker -l info
 
 
